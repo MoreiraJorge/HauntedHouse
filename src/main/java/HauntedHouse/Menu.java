b87594f142f74d793c95ad7df7d57c6131570e9d
MIGUEL ANGELO MARTINS DA COSTA <8160525@estg.ipp.pt>
2020-01-23 18:52:36 +0000

    Implement loadMap

---
+++ src/main/java/HauntedHouse/Menu.java
@@ -0,0 +1,40 @@
+package HauntedHouse;
+
+import java.io.FileReader;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class Menu {
+
+    private String name;
+    private int points;
+
+    public boolean loadMap(String fileLocation) {
+        JSONParser parser = new JSONParser();
+
+        try {
+            Object obj = parser.parse(new FileReader(fileLocation));
+            JSONObject jsonObject = (JSONObject) obj;
+
+            name = (String) jsonObject.get("nome");
+            long pointLong = (long) jsonObject.get("pontos");
+            points = Math.toIntExact(pointLong);
+
+            JSONArray map = (JSONArray) jsonObject.get("mapa");
+
+            System.out.println("Nome: " + name);
+            System.out.println("Pontos: " + points);
+            System.out.print("Mapa: ");
+            Iterator<JSONObject> iterator = map.iterator();
+            while (iterator.hasNext()) {
+                System.out.print("->" + iterator.next().get("aposento") + " ");
+            }
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
