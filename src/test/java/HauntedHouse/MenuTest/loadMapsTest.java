36425d3a8e8d3866543c9b9b90674fd3a14e5ca7
DevMig <8160525@estg.ipp.pt>
2020-01-23 19:24:44 +0000

    Implemented LoadMap (#6)
    * Add JsonSimple to Gradle
    
    * Json Pulgin Update
    
    * Maps Dir
    
    * Implement loadMap
    
    * Tests for loadMap

---
+++ src/test/java/HauntedHouse/MenuTest/loadMapsTest.java
@@ -0,0 +1,29 @@
+package HauntedHouse.MenuTest;
+
+import HauntedHouse.Menu;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class loadMapsTest {
+
+    Menu menu;
+    String fileLocation;
+
+    @BeforeEach
+    public void testSetup(){
+        menu = new Menu();
+    }
+
+    @Test
+    public void loadMapWhenFileLocationIsValid(){
+        fileLocation = "maps/mapa.json";
+        Assertions.assertTrue(menu.loadMap(fileLocation));
+    }
+
+    @Test
+    public void loadMapWhenFileLocationIsInValid(){
+        fileLocation = "maps/x.json";
+        Assertions.assertFalse(menu.loadMap(fileLocation));
+    }
+}
