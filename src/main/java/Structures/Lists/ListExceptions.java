789f6b4a076f0dfe8518611b32893ca5d4fffbef
8160297 <8160297@estg.ipp.pt>
2020-01-24 17:48:14 +0000

    add orderedList

--- src/main/java/Structures/Lists/ListExceptions.java
+++ src/main/java/Structures/Lists/ListExceptions.java
@@ -4,7 +4,7 @@ public class ListExceptions extends Exception {
 
     public static final String ELEMENT_NOT_FOUND = "Elemento não existe na lista.";
     public static final String EMPTY_LIST = "Elemento não existe na lista.";
-
+    public static final String OBJECT_NOT_COMPARABLE = "Objecto não é instancia de Comparable.";
 
     public ListExceptions() {
     }
