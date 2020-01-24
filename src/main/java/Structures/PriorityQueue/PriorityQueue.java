a2eab41aa25518cd3d35fa035e547a90da3a695d
MIGUEL ANGELO MARTINS DA COSTA <8160525@estg.ipp.pt>
2020-01-24 18:10:44 +0000

    Add PriorityQueue Package

---
+++ src/main/java/Structures/PriorityQueue/PriorityQueue.java
@@ -0,0 +1,21 @@
+package Structures.PriorityQueue;
+
+import Structures.BinaryTree.ArrayMinHeap;
+import Structures.BinaryTree.BinaryTreeExceptions;
+
+public class PriorityQueue<T> extends ArrayMinHeap<PriorityQueueNode<T>> {
+
+    public PriorityQueue() {
+    }
+
+    public void addElement(T object, int priority) {
+        PriorityQueueNode<T> node = new PriorityQueueNode<T>(object, priority);
+        super.addElement(node);
+    }
+
+    public T removeNext() throws BinaryTreeExceptions {
+        PriorityQueueNode<T> temp = (PriorityQueueNode<T>) super.removeMin();
+        return temp.getElement();
+    }
+
+}
