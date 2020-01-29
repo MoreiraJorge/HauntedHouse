7537a324a9f11e3b5e33a6f4b43e9820d6fe0bc2
MIGUEL ANGELO MARTINS DA COSTA <8160525@estg.ipp.pt>
2020-01-29 18:05:40 +0000

    Version 1.0 of Manual Game Mode

--- src/main/java/HauntedHouse/Game/Manual.java
+++ src/main/java/HauntedHouse/Game/Manual.java
@@ -1,11 +1,14 @@
 package HauntedHouse.Game;
 
 import HauntedHouse.MapDefinitions.Map;
+import HauntedHouse.MapDefinitions.MapExceptions;
 import HauntedHouse.MapDefinitions.Room;
+import Structures.Stack.EmptyCollectionException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 
 /**
  * Manual Gamemode class
@@ -35,11 +38,59 @@ public class Manual implements Game {
     }
 
     @Override
-    public void startGame(){
+    public void startGame() throws EmptyCollectionException, IOException, MapExceptions {
+        Room currentRoom = player.getCurrentRoom();
 
+        System.out.println("------------------------------------------------");
+        System.out.println("Começo do Jogo : \n");
+        while (true) {
+            map.printMapFromRoom(currentRoom.toString(), true);
+            System.out.println();
+            this.showPlayerPoints();
+            System.out.println("------------------------------------------------");
+            askNextRoom();
+            currentRoom = player.getCurrentRoom();
+            if (currentRoom.getRoomName().equalsIgnoreCase(Map.EXIT)) {
+                break;
+            }
+            System.out.println("------------------------------------------------");
+        }
+
+        System.out.println("\nFinal do jogo. Pontuação Final : " + this.player.getPlayerPoints() + "\n");
+    }
+
+    /**
+     * Method to ask the player for their next move
+     *
+     * @throws IOException
+     * @throws EmptyCollectionException
+     */
+    private void askNextRoom() throws IOException, EmptyCollectionException {
+        System.out.println("Introduza o nome da sala onde deseja ir: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input = reader.readLine();
+        Iterator<Room> connectedRoomsItr = player.getCurrentRoom().getConnectionsIterator();
+        Room selectedRoom = null;
+        boolean found = false;
+
+        while (connectedRoomsItr.hasNext() && !found) {
+            Room tmpRoom = connectedRoomsItr.next();
+            if (tmpRoom.getRoomName().equalsIgnoreCase(input)) {
+                selectedRoom = tmpRoom;
+                found = true;
+            }
+        }
+
+        if (found) {
+            player.makeMove(selectedRoom);
+            player.setPlayerPoints(player.getPlayerPoints() - (diff * selectedRoom.getGhostCost()));
+        } else {
+            System.out.println("Escolha inválida.");
+        }
     }
 
     @Override
     public void showPlayerPoints() {
+        System.out.println("Pontuação Atual: " + player.getPlayerPoints());
     }
 }
