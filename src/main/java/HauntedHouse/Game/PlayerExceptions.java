1669b5d9ec9f07e3c7d290124340650d81a674b8
MIGUEL ANGELO MARTINS DA COSTA <8160525@estg.ipp.pt>
2020-01-31 18:14:35 +0000

    Add Player Exceptions

--- src/main/java/HauntedHouse/Game/PlayerExceptions.java
+++ src/main/java/HauntedHouse/Game/PlayerExceptions.java
@@ -1,4 +1,27 @@
 package HauntedHouse.Game;
 
-public class PlayerExceptions {
+/**
+ * Class with exceptions to use on Player
+ * class
+ *
+ * @author Jorge, Miguel
+ */
+public class PlayerExceptions extends Exception {
+
+    /**
+     * message for when flash cannot be used
+     */
+    public static final String FLASHBACK_CANNOT_BE_USED = "Não pode usar o flashback porque se encontra na entrada.";
+    /**
+     * message for when number of flashback is zero
+     */
+    public static final String NUMBER_OF_FLASHBACK_IS_ZERO = "Já esgotou os flashback disponíveis.";
+
+    public PlayerExceptions() {
+    }
+
+    public PlayerExceptions(String message) {
+        super(message);
+    }
+
 }
