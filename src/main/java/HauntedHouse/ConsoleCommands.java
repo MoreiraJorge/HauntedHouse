2735d77b1480ea1b1a2bda7147707ae0a9bb86a1
MIGUEL ANGELO MARTINS DA COSTA <8160525@estg.ipp.pt>
2020-01-31 18:13:56 +0000

    Commands to clean console

---
+++ src/main/java/HauntedHouse/ConsoleCommands.java
@@ -0,0 +1,34 @@
+package HauntedHouse;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+/**
+ * Commands for console application
+ */
+public class ConsoleCommands {
+
+    /**
+     * Command to clean console
+     */
+    public final static void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (final Exception e) {
+            Robot robot = null;
+            try {
+                robot = new Robot();
+                robot.keyPress(KeyEvent.VK_PAUSE);
+            } catch (AWTException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+}
